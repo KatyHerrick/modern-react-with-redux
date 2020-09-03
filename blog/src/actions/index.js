ffbe721c8b6356351565c4e626e77a891806cec5
Katy Herrick <kmh530@nyu.edu>
2020-09-03 10:17:12 -0400

    add example of a memoized async action

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Despite the syntax, this just returns an async function
@@ -9,8 +10,11 @@ export const fetchPosts = () => async dispatch => {
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+// Returns a memoized function that dispatches an action with the
+// results of an async API call.
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
